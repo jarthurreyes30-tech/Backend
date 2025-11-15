5f461074b0c74e82c60e90ce3ed7296601e12e85
aaronsagan <saganaarondave33@gmail.com>
2025-11-16 01:01:49 +0800

    fix: Add localhost:8082 to CORS allowed origins for charity registration

--- capstone_backend/config/cors.php
+++ capstone_backend/config/cors.php
@@ -24,6 +24,8 @@ return [
         'http://127.0.0.1:8080',
         'http://localhost:8081',
         'http://127.0.0.1:8081',
+        'http://localhost:8082',
+        'http://127.0.0.1:8082',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:3000',
